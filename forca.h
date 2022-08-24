b1eff9bd3777e909835924ef19ae4110f04096cc
Jelson Jr <96444665+JelsonJr@users.noreply.github.com>
2022-08-24 11:30:31 -0300

    Commit dos arquivos

---
+++ forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 30
+
+void abertura();
+void chuta();
+int jaChutou(char letra);
+void desenharForca();
+void adicionaPalavra();
+void revelaPalavraComposta();
+void mostraLetrasChutadas(erros);
+int acertou();
+int enforcou();
